66199296873c397f0b229339593bb15abe88efcd
komesergey <komesergey@gmail.com>
2018-10-09 00:11:29 +0300

    Refactoring

--- accumulator.cpp
+++ accumulator.cpp
@@ -3,44 +3,35 @@
 //
 #include <functional>
 #include <iostream>
-
 using namespace std;
-
-//using decay_t = typename decay<T>::type;
-
-//If T is a function type, a function-to-pointer conversion is applied and the decay type is the same as: add_pointer<T>::type
-//If T is an array type, an array-to-pointer conversion is applied and the decay type is the same as: add_pointer<remove_extent<remove_reference<T>::type>::type>::type
-//Otherwise, a regular lvalue-to-rvalue conversion is applied and the decay type is the same as: remove_cv<remove_reference<T>::type>::type.
-
-//typedef std::decay<int>::type A;           // int
-//typedef std::decay<int&>::type B;          // int
-//typedef std::decay<int&&>::type C;         // int
-//typedef std::decay<const int&>::type D;    // int
-//typedef std::decay<int[2]>::type E;        // int*
-//typedef std::decay<int(int)>::type F;      // int(*)(int)
-
+//container for a function/lambda
+//T - type of function, std::function for example
 template<typename T>
-struct recallable_impl {
+struct recallable {
+    //template with template parameter pack
     template<typename... Args>
-    recallable_impl& operator()(Args&&... args) {
+    //overloaded method receives arguments to original function
+    recallable& operator()(Args&&... args) {
+        //and invokes function with it
         f(std::forward<Args>(args)...);
+        //then returns itself as a container for function
         return *this;
     }
-    template<typename F> recallable_impl(F&& f):f{std::forward<F>(f)} {}
+    //constructor which receives function/lambda and captures it
+    explicit recallable(const T& f) : f{f} {}
 private:
-    T f;
+    //reference to function
+    const T& f;
 };
 
-template<typename T>
-decltype(auto) recallable(T&& f) {
-    return recallable_impl<std::decay_t<T>>(std::forward<T>(f));
-}
+//function receives lambda and returns recallable structure
+template<typename T> recallable<T> r(const T& f) {return recallable<T>{f};}
 
 int main()
 {
     int a = 0;
     auto l1 = [&](int b) { a+=b; };
-    recallable(l1)(1)(2)(3)(4)(5);
+    r(l1)(1)(2)(3)(4)(5);
     std::cout << a << std::endl;
     return 0;
 }
