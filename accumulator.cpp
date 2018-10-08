ae74e1bbb47656a8344aed00272db579937d2cf6
komesergey <komesergey@gmail.com>
2018-10-09 00:20:00 +0300

    Refactoring

--- accumulator.cpp
+++ accumulator.cpp
@@ -4,33 +4,33 @@
 #include <functional>
 #include <iostream>
 using namespace std;
-//container for a function/lambda
-//T - type of function, std::function for example
+//container for a lambda
+//T - type of target function, std::function<void(int)> in this example
 template<typename T>
-struct recallable {
+struct callable {
     //template with template parameter pack
     template<typename... Args>
-    //overloaded method receives arguments to original function
-    recallable& operator()(Args&&... args) {
-        //and invokes function with it
+    //overloaded method receives arguments to target function
+    callable& operator()(Args&&... args) {
+        //and invokes target function with them
         f(std::forward<Args>(args)...);
-        //then returns itself as a container for function
+        //then returns itself as a container for target function
         return *this;
     }
     //constructor which receives function/lambda and captures it
-    explicit recallable(const T& f) : f{f} {}
+    explicit callable(const T& f) : f{f} {}
 private:
     //reference to function
     const T& f;
 };
 
 //function receives lambda and returns recallable structure
-template<typename T> recallable<T> r(const T& f) {return recallable<T>{f};}
+template<typename T> callable<T> r(const T& f) {return callable<T>{f};}
 
 int main()
 {
     int a = 0;
-    auto l1 = [&](int b) { a+=b; };
+    std::function<void(int)> l1 = [&](int b) { a+=b; };
     r(l1)(1)(2)(3)(4)(5);
     std::cout << a << std::endl;
     return 0;
