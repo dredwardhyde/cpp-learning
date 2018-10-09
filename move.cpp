12582bf44133968ba49723b1656314056a577079
komesergey <komesergey@gmail.com>
2018-10-09 23:16:39 +0300

    Updated example with std::move

--- move.cpp
+++ move.cpp
@@ -17,21 +17,33 @@ template <typename T> void swapWithCopying(T& a, T& b)
 
 template <typename T> void swapWithoutCopying(T& a, T& b)
 {
-    // Roughly equivalent to static_cast<T&&>(t);
-    // As a cast, it does not do anything during runtime.
-    // It is only relevant at compile time to tell the compiler
+    // roughly equivalent to static_cast<T&&>(t);
+    // as a cast, it does not do anything during runtime.
+    // it is only relevant at compile time to tell the compiler
     // that you would like to continue considering the reference as an rvalue.
 
-    // What it does not do:
-    // - Make a copy of the argument
-    // - Call the copy constructor
-    // - Change the argument object
+    // what it does not do:
+    // - make a copy of the argument
+    // - call the copy constructor
+    // - change the argument object
 
     T tmp(std::move(a));
     std::cout << std::endl << "Now \"a\" is empty: " << a << std::endl;
     a = std::move(b);
     std::cout << "Now \"b\" is empty: " << b << std::endl;
     b = std::move(tmp);
+    std::cout << "Now \"tmp\" is empty: " << tmp << std::endl << std::endl;
+}
+
+template <typename T> void swapWithCast(T& a, T& b)
+{
+    // now test with static_cast<T&&>(t); instead of std::move
+    // same results as with std::move
+    T tmp(static_cast<T&&>(a));
+    std::cout << std::endl << "Now \"a\" is empty: " << a << std::endl;
+    a = static_cast<T&&>(b);
+    std::cout << "Now \"b\" is empty: " << b << std::endl;
+    b = static_cast<T&&>(tmp);
     std::cout << "Now \"tmp\" is empty: " << tmp << std::endl << std::endl;
 }
 
@@ -60,6 +72,8 @@ int main(){
     std::cout << "After first swap: a = " << a << " b = " << b << std::endl;
     swapWithoutCopying(a, b);
     std::cout << "After second swap: a = " << a << " b = " << b << std::endl;
+    swapWithCast(a, b);
+    std::cout << "After third swap: a = " << a << " b = " << b << std::endl;
 
 }
 
