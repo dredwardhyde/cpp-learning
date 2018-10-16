7a67301ac54d4805effc96b01c0c6b982a6b6af0
komesergey <komesergey@gmail.com>
2018-10-16 13:14:36 +0300

    Updated with scoped_thread

--- thread_guard.cpp
+++ thread_guard.cpp
@@ -21,6 +21,21 @@ public:
     thread_guard& operator=(thread_guard const&)=delete;
 };
 
+class scoped_thread {
+private:
+    std::thread t;
+public:
+    explicit scoped_thread(std::thread t_): t(std::move(t_)) {
+        if(!t.joinable()) throw std::logic_error("No thread");
+    }
+    ~scoped_thread() {
+        std::cout << "Joined\n";
+        t.join();
+    }
+    scoped_thread(scoped_thread const&)=delete;
+    scoped_thread& operator=(scoped_thread const&)=delete;
+};
+
 
 void print(std::string& str){
     std::cout << str;
@@ -39,10 +54,21 @@ void printMain(){
           before calling terminate() is implementation-defined.
  */
 int main(){
+
+    std::cout << "thread_guard without std::moving of thread" << std::endl;
     try {
         std::string str = "From child thread\n";
         std::thread t(print, std::ref(str));
         thread_guard g(t);
+        printMain();
+    }catch (std::exception& e){
+        std::cout << e.what() << std::endl;
+    }
+
+    std::cout  << std::endl << "scoped_thread with std::moving of thread" << std::endl;
+    try {
+        std::string str = "From child thread\n";
+        scoped_thread g(std::thread(print, std::ref(str)));
         printMain();
     }catch (std::exception& e){
         std::cout << e.what() << std::endl;
