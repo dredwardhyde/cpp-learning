3b02ebafae4cfdc92a905efa6d4ac76d7a302455
komesergey <komesergey@gmail.com>
2018-10-25 21:30:50 +0300

    Added example of thread-safe queue with fine-grained locking

---
+++ threadsafe_queue_tuned.h
@@ -0,0 +1,116 @@
+//
+// Created by Edward Hyde on 25/10/2018.
+//
+
+#ifndef CPP_LEARNING_THREADSAFE_QUEUE_TUNED_H
+#define CPP_LEARNING_THREADSAFE_QUEUE_TUNED_H
+
+#include <memory>
+#include <mutex>
+
+
+template<typename T>
+class threadsafe_queue_tuned {
+private:
+
+    struct node {
+        std::shared_ptr<T> data;
+        std::unique_ptr<node> next;
+    };
+
+    std::mutex head_mutex;
+    std::unique_ptr<node> head;
+    std::mutex tail_mutex;
+    node* tail;
+    std::condition_variable data_cond;
+
+    node* get_tail() {
+        std::lock_guard<std::mutex> tail_lock(tail_mutex);
+        return tail;
+    }
+
+    std::unique_ptr<node> pop_head() {
+        std::unique_ptr<node> old_head = std::move(head);
+        head = std::move(old_head->next);
+        return old_head;
+    }
+
+    std::unique_lock<std::mutex> wait_for_data() {
+        std::unique_lock<std::mutex> head_lock(head_mutex);
+        data_cond.wait(head_lock,[&]{return head.get() != get_tail();});
+        return std::move(head_lock);
+    }
+
+    std::unique_ptr<node> wait_pop_head() {
+        std::unique_lock<std::mutex> head_lock(wait_for_data());
+        return pop_head();
+    }
+
+    std::unique_ptr<node> wait_pop_head(T& value) {
+        std::unique_lock<std::mutex> head_lock(wait_for_data());
+        value = std::move(*head->data);
+        return pop_head();
+    }
+
+    std::unique_ptr<node> try_pop_head() {
+        std::lock_guard<std::mutex> head_lock(head_mutex);
+        if(head.get() == get_tail()) {
+            return std::unique_ptr<node>();
+        }
+        return pop_head();
+    }
+
+    std::unique_ptr<node> try_pop_head(T& value) {
+        std::lock_guard<std::mutex> head_lock(head_mutex);
+        if(head.get() == get_tail())
+        {
+            return std::unique_ptr<node>();
+        }
+        value = std::move(*head->data);
+        return pop_head();
+    }
+
+public:
+    threadsafe_queue_tuned(): head(new node),tail(head.get()) {}
+    threadsafe_queue_tuned(const threadsafe_queue_tuned& other) = delete;
+    threadsafe_queue_tuned& operator=(const threadsafe_queue_tuned& other) = delete;
+
+    void push(T new_value) {
+        std::shared_ptr<T> new_data(std::make_shared<T>(std::move(new_value)));
+        std::unique_ptr<node> p(new node);
+        {
+            std::lock_guard<std::mutex> tail_lock(tail_mutex);
+            tail->data = new_data;
+            node* const new_tail = p.get();
+            tail->next = std::move(p);
+            tail = new_tail;
+        }
+        data_cond.notify_one();
+    }
+
+    std::shared_ptr<T> wait_and_pop() {
+        std::unique_ptr<node> const old_head=wait_pop_head();
+        return old_head->data;
+    }
+
+    void wait_and_pop(T& value) {
+        std::unique_ptr<node> const old_head=wait_pop_head(value);
+    }
+
+    std::shared_ptr<T> try_pop() {
+        std::unique_ptr<node> old_head = try_pop_head();
+        return old_head ? old_head->data:std::shared_ptr<T>();
+    }
+
+    bool try_pop(T& value) {
+        std::unique_ptr<node> const old_head = try_pop_head(value);
+        return old_head;
+    }
+
+    bool empty() {
+        std::lock_guard<std::mutex> head_lock(head_mutex);
+        return (head.get() == get_tail());
+    }
+};
+
+#endif //CPP_LEARNING_THREADSAFE_QUEUE_TUNED_H
