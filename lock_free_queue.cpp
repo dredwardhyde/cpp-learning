077881b63c3860a46ef426da0856263278757f1f
komesergey <komesergey@gmail.com>
2018-11-04 03:57:31 +0300

    Fixed for Windows for DEBUG. In Release config execution fails because compare_exchange_strong fails somehow.

--- lock_free_queue.cpp
+++ lock_free_queue.cpp
@@ -5,31 +5,26 @@
 #include <thread>
 
 template<typename T>
-class queue
-{
+class queue {
 private:
     struct node;
 
-    struct counted_node_ptr
-    {
+    struct counted_node_ptr {
         int external_count;
         node* ptr;
     };
 
-    struct node_counter
-    {
+    struct node_counter {
         int internal_count:30;
         unsigned external_counters:2;
     };
 
-    struct node
-    {
+    struct node {
         std::atomic<T*> data;
         std::atomic<node_counter> count;
         std::atomic<counted_node_ptr> next;
 
-        node()
-        {
+        node() {
             node_counter new_count;
             new_count.internal_count=0;
             new_count.external_counters=2;
@@ -39,99 +34,66 @@ private:
             next.store(next_node);
         }
 
-        void release_ref()
-        {
+        void release_ref() {
             node_counter old_counter=
                     count.load(std::memory_order_relaxed);
             node_counter new_counter;
-            do
-            {
+            do {
                 new_counter=old_counter;
                 --new_counter.internal_count;
-            }
-            while(!count.compare_exchange_strong(
-                    old_counter,new_counter,
-                    std::memory_order_acquire,std::memory_order_relaxed));
+            } while(!count.compare_exchange_strong(old_counter,new_counter, std::memory_order_acquire,std::memory_order_relaxed));
 
-            if(!new_counter.internal_count &&
-               !new_counter.external_counters)
-            {
+            if(!new_counter.internal_count && !new_counter.external_counters) {
                 delete this;
             }
         }
-
     };
 
     std::atomic<counted_node_ptr> head;
     std::atomic<counted_node_ptr> tail;
 
-    static void increase_external_count(
-            std::atomic<counted_node_ptr>& counter,
-            counted_node_ptr& old_counter)
-    {
+    static void increase_external_count(std::atomic<counted_node_ptr>& counter, counted_node_ptr& old_counter) {
         counted_node_ptr new_counter;
-
-        do
-        {
+        do {
             new_counter=old_counter;
             ++new_counter.external_count;
-        }
-        while(!counter.compare_exchange_strong(
-                old_counter,new_counter,
-                std::memory_order_acquire,
-                std::memory_order_relaxed));
-
+        } while(!counter.compare_exchange_strong(old_counter,new_counter, std::memory_order_acquire, std::memory_order_relaxed));
         old_counter.external_count=new_counter.external_count;
     }
 
-    void set_new_tail(counted_node_ptr &old_tail,
-                      counted_node_ptr const &new_tail)
-    {
+    void set_new_tail(counted_node_ptr &old_tail, counted_node_ptr const &new_tail) {
         node* const current_tail_ptr=old_tail.ptr;
-        while(!tail.compare_exchange_weak(old_tail,new_tail) &&
-              old_tail.ptr==current_tail_ptr);
-        if(old_tail.ptr==current_tail_ptr)
-        {
+        while(!tail.compare_exchange_weak(old_tail,new_tail) && old_tail.ptr==current_tail_ptr);
+        if(old_tail.ptr==current_tail_ptr) {
             free_external_counter(old_tail);
-        }
-        else
-        {
+        } else {
             current_tail_ptr->release_ref();
         }
     }
 
-    static void free_external_counter(counted_node_ptr &old_node_ptr)
-    {
+    static void free_external_counter(counted_node_ptr &old_node_ptr) {
         node* const ptr=old_node_ptr.ptr;
         int const count_increase=old_node_ptr.external_count-2;
 
         node_counter old_counter=
                 ptr->count.load(std::memory_order_relaxed);
         node_counter new_counter;
-        do
-        {
+        do {
             new_counter=old_counter;
             --new_counter.external_counters;
             new_counter.internal_count+=count_increase;
-        }
-        while(!ptr->count.compare_exchange_strong(
-                old_counter,new_counter,
-                std::memory_order_acquire,std::memory_order_relaxed));
+        } while(!ptr->count.compare_exchange_strong(old_counter,new_counter, std::memory_order_acquire,std::memory_order_relaxed));
 
-        if(!new_counter.internal_count &&
-           !new_counter.external_counters)
-        {
+        if(!new_counter.internal_count && !new_counter.external_counters) {
             delete ptr;
         }
     }
 
 public:
-    queue()
-    {
+    queue() {
         counted_node_ptr new_node;
         new_node.external_count=1;
         new_node.ptr=new node;
-
         head.store(new_node);
         tail.store(head.load());
     }
@@ -139,25 +101,20 @@ public:
     queue(const queue& other)=delete;
     queue& operator=(const queue& other)=delete;
 
-    ~queue()
-    {
+    ~queue() {
         while(pop());
         delete head.load().ptr;
     }
-    std::unique_ptr<T> pop()
-    {
+    std::unique_ptr<T> pop() {
         counted_node_ptr old_head=head.load(std::memory_order_relaxed);
-        for(;;)
-        {
+        for(;;) {
             increase_external_count(head,old_head);
             node* const ptr=old_head.ptr;
-            if(ptr==tail.load().ptr)
-            {
+            if(ptr==tail.load().ptr) {
                 return std::unique_ptr<T>();
             }
             counted_node_ptr next=ptr->next.load();
-            if(head.compare_exchange_strong(old_head,next))
-            {
+            if(head.compare_exchange_strong(old_head,next)) {
                 T* const res=ptr->data.exchange(nullptr);
                 free_external_counter(old_head);
                 return std::unique_ptr<T>(res);
@@ -166,39 +123,30 @@ public:
         }
     }
 
-    void push(T new_value)
-    {
+    void push(T new_value) {
         std::unique_ptr<T> new_data(new T(new_value));
         counted_node_ptr new_next;
         new_next.ptr=new node;
         new_next.external_count=1;
         counted_node_ptr old_tail=tail.load();
 
-        for(;;)
-        {
+        for(;;) {
             increase_external_count(tail,old_tail);
 
             T* old_data= nullptr;
-            if(old_tail.ptr->data.compare_exchange_strong(
-                    old_data,new_data.get()))
-            {
+            if(old_tail.ptr->data.compare_exchange_strong(old_data,new_data.get())) {
                 counted_node_ptr old_next={0, nullptr};
-                if(!old_tail.ptr->next.compare_exchange_strong(
-                        old_next,new_next))
-                {
+                if(!old_tail.ptr->next.compare_exchange_strong(old_next,new_next)) {
+                    std::cout << "Wrong path in Release";
                     delete new_next.ptr;
                     new_next=old_next;
                 }
                 set_new_tail(old_tail, new_next);
                 new_data.release();
                 break;
-            }
-            else
-            {
+            } else {
                 counted_node_ptr old_next={0, nullptr};
-                if(old_tail.ptr->next.compare_exchange_strong(
-                        old_next,new_next))
-                {
+                if(old_tail.ptr->next.compare_exchange_strong(old_next,new_next)) {
                     old_next=new_next;
                     new_next.ptr=new node;
                 }
@@ -209,7 +157,7 @@ public:
 };
 
 void producer(queue<int>* stack){
-    for(int i = 0; i < 1000;i++){
+    for(int i = 0; i < 10;i++){
         stack->push(i);
     }
 }
@@ -224,21 +172,21 @@ int main(){
     auto* stack = new queue<int>();
     auto start = std::chrono::high_resolution_clock::now();
     std::thread a(producer, std::ref(stack));
-    std::thread b(producer, std::ref(stack));
-    std::thread e(producer, std::ref(stack));
+//    std::thread b(producer, std::ref(stack));
+//    std::thread e(producer, std::ref(stack));
     a.join();
-    b.join();
-    e.join();
+//    b.join();
+//    e.join();
     auto done = std::chrono::high_resolution_clock::now();
     long resultTime = std::chrono::duration_cast<std::chrono::nanoseconds>(done - start).count();
     std::cout << "Pushing: " << resultTime << std::endl;
     start = std::chrono::high_resolution_clock::now();
     std::thread c(consumer, std::ref(stack));
-    std::thread d(consumer, std::ref(stack));
-    std::thread f(consumer, std::ref(stack));
+//    std::thread d(consumer, std::ref(stack));
+//    std::thread f(consumer, std::ref(stack));
     c.join();
-    d.join();
-    f.join();
+//    d.join();
+//    f.join();
     done = std::chrono::high_resolution_clock::now();
     resultTime = std::chrono::duration_cast<std::chrono::nanoseconds>(done - start).count();
     std::cout << "Popping: " << resultTime << std::endl;
