816c7a93f11a00229a41070e1ba2306a89b48d40
komesergey <komesergey@gmail.com>
2018-11-04 12:06:37 +0300

    Added VLD support for Windows. No leaks detected. Fixed style

--- lock_free_queue.cpp
+++ lock_free_queue.cpp
@@ -1,3 +1,6 @@
+#ifdef _MSC_VER
+    #include <vld.h>
+#endif
 #include <atomic>
 #include <memory>
 #include <chrono>
@@ -26,21 +29,18 @@ private:
         std::atomic<counted_node_ptr> next;
 
         node() {
-            node_counter new_count;
-            new_count.internal_count=0;
-            new_count.external_counters=2;
+            node_counter new_count = {0, 2};
             count.store(new_count);
             data = nullptr;
-            counted_node_ptr next_node={0, 0, nullptr};
+            counted_node_ptr next_node = {0, 0, nullptr};
             next.store(next_node);
         }
 
         void release_ref() {
-            node_counter old_counter=
-                    count.load(std::memory_order_relaxed);
+            node_counter old_counter = count.load(std::memory_order_relaxed);
             node_counter new_counter;
             do {
-                new_counter=old_counter;
+                new_counter = old_counter;
                 --new_counter.internal_count;
             } while(!count.compare_exchange_strong(old_counter,new_counter, std::memory_order_acquire,std::memory_order_relaxed));
 
@@ -56,16 +56,16 @@ private:
     static void increase_external_count(std::atomic<counted_node_ptr>& counter, counted_node_ptr& old_counter) {
         counted_node_ptr new_counter;
         do {
-            new_counter=old_counter;
+            new_counter = old_counter;
             ++new_counter.external_count;
         } while(!counter.compare_exchange_strong(old_counter,new_counter, std::memory_order_acquire, std::memory_order_relaxed));
         old_counter.external_count=new_counter.external_count;
     }
 
     void set_new_tail(counted_node_ptr &old_tail, counted_node_ptr const &new_tail) {
-        node* const current_tail_ptr=old_tail.ptr;
-        while(!tail.compare_exchange_weak(old_tail,new_tail) && old_tail.ptr==current_tail_ptr);
-        if(old_tail.ptr==current_tail_ptr) {
+        node* const current_tail_ptr = old_tail.ptr;
+        while(!tail.compare_exchange_weak(old_tail,new_tail) && old_tail.ptr == current_tail_ptr);
+        if(old_tail.ptr == current_tail_ptr) {
             free_external_counter(old_tail);
         } else {
             current_tail_ptr->release_ref();
@@ -73,18 +73,15 @@ private:
     }
 
     static void free_external_counter(counted_node_ptr &old_node_ptr) {
-        node* const ptr=old_node_ptr.ptr;
-        int const count_increase=old_node_ptr.external_count-2;
-
-        node_counter old_counter=
-                ptr->count.load(std::memory_order_relaxed);
+        node* const ptr = old_node_ptr.ptr;
+        int const count_increase = old_node_ptr.external_count-2;
+        node_counter old_counter = ptr->count.load(std::memory_order_relaxed);
         node_counter new_counter;
         do {
-            new_counter=old_counter;
+            new_counter = old_counter;
             --new_counter.external_counters;
-            new_counter.internal_count+=count_increase;
+            new_counter.internal_count += count_increase;
         } while(!ptr->count.compare_exchange_strong(old_counter,new_counter, std::memory_order_acquire,std::memory_order_relaxed));
-
         if(!new_counter.internal_count && !new_counter.external_counters) {
             delete ptr;
         }
@@ -92,31 +89,29 @@ private:
 
 public:
     queue() {
-        counted_node_ptr new_node;
-        new_node.external_count=1;
-        new_node.ptr=new node;
+        counted_node_ptr new_node = {1, 0, new node};
         head.store(new_node);
         tail.store(head.load());
     }
 
-    queue(const queue& other)=delete;
-    queue& operator=(const queue& other)=delete;
+    queue(const queue& other) = delete;
+    queue& operator=(const queue& other) = delete;
 
     ~queue() {
         while(pop());
         delete head.load().ptr;
     }
     std::unique_ptr<T> pop() {
-        counted_node_ptr old_head=head.load(std::memory_order_relaxed);
+        counted_node_ptr old_head = head.load(std::memory_order_relaxed);
         for(;;) {
             increase_external_count(head,old_head);
-            node* const ptr=old_head.ptr;
-            if(ptr==tail.load().ptr) {
+            node* const ptr = old_head.ptr;
+            if(ptr == tail.load().ptr) {
                 return std::unique_ptr<T>();
             }
-            counted_node_ptr next=ptr->next.load();
+            counted_node_ptr next = ptr->next.load();
             if(head.compare_exchange_strong(old_head,next)) {
-                T* const res=ptr->data.exchange(nullptr);
+                T* const res = ptr->data.exchange(nullptr);
                 free_external_counter(old_head);
                 return std::unique_ptr<T>(res);
             }
@@ -126,29 +121,27 @@ public:
 
     void push(T new_value) {
         std::unique_ptr<T> new_data(new T(new_value));
-        counted_node_ptr new_next;
-        new_next.ptr=new node;
-        new_next.external_count=1;
-        counted_node_ptr old_tail=tail.load();
+        counted_node_ptr new_next = { 1, 0, new node};
+        counted_node_ptr old_tail = tail.load();
 
         for(;;) {
             increase_external_count(tail,old_tail);
-
-            T* old_data= nullptr;
+            T* old_data = nullptr;
             if(old_tail.ptr->data.compare_exchange_strong(old_data,new_data.get())) {
-                counted_node_ptr old_next={0, 0, nullptr};
+                counted_node_ptr old_next = {0, 0, nullptr};
                 if(!old_tail.ptr->next.compare_exchange_strong(old_next,new_next)) {
                     delete new_next.ptr;
-                    new_next=old_next;
+                    new_next = old_next;
                 }
                 set_new_tail(old_tail, new_next);
                 new_data.release();
                 break;
             } else {
-                counted_node_ptr old_next={0, 0, nullptr};
+                counted_node_ptr old_next = {0, 0, nullptr};
                 if(old_tail.ptr->next.compare_exchange_strong(old_next,new_next)) {
-                    old_next=new_next;
-                    new_next.ptr=new node;
+                    old_next = new_next;
+                    // Possible memory leak here?
+                    new_next.ptr = new node;
                 }
                 set_new_tail(old_tail, old_next);
             }
